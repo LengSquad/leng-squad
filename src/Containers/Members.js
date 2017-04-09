90edb81391ee64d13888e5ea0d8b9110e9bccb8c
Martin Stigen <Martin Stigen>
2017-04-10 01:04:00 +0200

    Now showing member in nested list

--- src/Containers/Members.js
+++ src/Containers/Members.js
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import './Members.css';
 
-import Subheader from 'material-ui/Subheader';
+import {List, ListItem} from 'material-ui/List';
+
+const rankMapping = {
+    0: "Guild Master",
+    1: "Officer",
+    2: "Officer Alt",
+    3: "Leng",
+    4: "Raider",
+    5: "Trialist",
+    6: "Social",
+    7: "Alt"
+};
 
 class Members extends Component {
 
@@ -18,26 +29,47 @@ class Members extends Component {
             this.setState({members});
         }
     }
-
+    
     render() {
 
-        let lastRank = 0;
+        let lastRank = -1;
 
         console.log(this.state.members[0]);
+
+        let ranks = [];
+
         for (let i in this.state.members) {
             const member = this.state.members[i];
             if (member.rank > lastRank) {
-                console.log(lastRank)
                 lastRank = member.rank;
+                ranks[lastRank] = [member];
             }
             else {
-
+                ranks[lastRank].push(member);
             }
         }
 
+        let members = ranks.map((rankList, i) => {
+
+            let list = rankList.map(member => {
+                return <ListItem
+                    key={member.character.name}
+                    primaryText={member.character.name}
+                />
+            });
+
+            return <ListItem
+                key={i}
+                primaryTogglesNestedList={true}
+                primaryText={rankMapping[i]}
+                nestedItems={list}
+            />
+        });
+
         return (
-            <div>
-            </div>
+            <List>
+                {members}
+            </List>
         );
     }
 
